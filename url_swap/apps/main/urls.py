727103673c14b503dfea966b735d9b483e97b34b
kamil <kamil.kozik@outlook.com>
2017-04-11 13:09:56 +0200

    Start main app.

---
+++ url_swap/apps/main/urls.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+
+
+urlpatterns = [
+    # TODO(KK): Provide a view for below url
+    url(r'^$', view='', name='show_form')
+]
--- url_swap/settings/base.py
+++ url_swap/settings/base.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # url_swap
+    'url_swap.apps.main'
 ]
 
 MIDDLEWARE = [
