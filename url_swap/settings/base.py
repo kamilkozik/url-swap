954333f016177688b17828f7ccddd3212a1148d2
kamil <kamil.kozik@outlook.com>
2017-04-11 16:26:01 +0200

    MySQL connected.

--- url_swap/settings/base.py
+++ url_swap/settings/base.py
@@ -86,11 +86,14 @@ WSGI_APPLICATION = 'url_swap.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'urlswap',
+        'USER': 'kkamil',
+        'PASSWORD': 'zaq12wsx',
+        'HOST': 'kkamil.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
