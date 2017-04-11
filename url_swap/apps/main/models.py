42991bed4f3e64e53d38f57827204fbef9fd2858
kamil <kamil.kozik@outlook.com>
2017-04-11 13:52:52 +0200

    [migration] UrlSwap model added

--- url_swap/apps/main/models.py
+++ url_swap/apps/main/models.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models
+import random
+import string
 
-# Create your models here.
+from django.db import models
+from six import python_2_unicode_compatible
+
+
+@python_2_unicode_compatible
+class UrlSwap(models.Model):
+    origin_url = models.URLField(u'Url oryginalny', max_length=2048, blank=False, null=False)
+    proxy_url = models.URLField(u'Proxy url', max_length=100, blank=False, null=False, db_index=True)
+
+    def __str__(self):
+        return self.origin
+
+    @staticmethod
+    def get_proxy_url(size):
+        return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(size))
